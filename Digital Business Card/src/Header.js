0210734b1472f0576c13a719f95b3d931894f106
Emre Gundogan <emregundogan5@gmail.com>
2022-09-21 16:44:43 +0300

    first commit

---
+++ Digital Business Card/src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="image--holder"></div>
+      <div className="info">
+      <h1 className="user--name">Emre Gündoğan</h1>
+      <h2 className="user--title">Frontend Developer</h2>
+      <h3 className="user--website">emregundogan.website</h3>
+      </div>
+    </div>
+  );
+}
