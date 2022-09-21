ef3f02de4e2955960809e41777c4457b2aa221be
Emre Gundogan <emregundogan5@gmail.com>
2022-09-21 16:44:08 +0300

    first commit

---
+++ Digital Business Card/src/Buttons.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Buttons() {
+  return (
+    <div className="buttons">
+      <a
+        className="button linkedInBtn"
+        target="_blank"
+        href="https://www.linkedin.com/in/gundoganemre/"
+      >
+        LinkedIn
+      </a>
+
+      <a className="button emailBtn" href="mailto:email@gmail.com">
+        Email
+      </a>
+    </div>
+  );
+}
