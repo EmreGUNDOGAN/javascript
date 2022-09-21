f5334120582098567b0e2c1dc24ee92ea91dca4a
Emre Gundogan <emregundogan5@gmail.com>
2022-09-21 16:45:02 +0300

    first commit

---
+++ Digital Business Card/src/Main.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div className="main">
+      <h2 className="main--title">About</h2>
+      <p className="main--par">
+        I'm a frontend Developer with a particular interest in making things
+        simple and automating daily tasks. I try to keep up with secutiry and
+        best practices, and am always looking for new things to learn.
+      </p>
+      <h2 className="main--title">Interests</h2>
+      <p className="main--par">
+        Love eating, watching movies regularly, travelling and playing games
+        with my girlfriend, shopping and at last in love with music.
+      </p>
+    </div>
+  );
+}
