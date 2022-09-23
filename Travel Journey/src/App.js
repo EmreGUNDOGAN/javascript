480c48b750e1da637bb45fb2a907e3f1b2ee7a7f
Emre Gundogan <emregundogan5@gmail.com>
2022-09-23 16:44:16 +0300

    first commit

---
+++ Travel Journey/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Data from "./components/Data";
+import Main from "./components/Main";
+import Header from "./components/Header";
+
+function App() {
+  const datas = Data;
+  const data = datas.map((item) => {
+    return <Main key={item.id} item={item} />;
+  });
+  console.log(data);
+
+  return (
+    <div className="container">
+       <Header />
+      {data}
+    </div>
+  );
+}
+
+export default App;
