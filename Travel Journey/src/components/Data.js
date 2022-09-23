ff6170590a526754a12778185a62203d846ee936
Emre Gundogan <emregundogan5@gmail.com>
2022-09-23 16:48:03 +0300

    first commit

---
+++ Travel Journey/src/components/Data.js
@@ -0,0 +1,48 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+  {
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+  },
+  {
+    title: "Sydney Opera House",
+    location: "Australia",
+    googleMapsUrl: "https://goo.gl/maps/Gq7xEfWt3GmHSXWs7",
+    startDate: "27 May, 2021",
+    endDate: "8 Jun, 2021",
+    description:
+      "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+    imageUrl:
+      "https://images.unsplash.com/photo-1523059623039-a9ed027e7fad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1432&q=80",
+  },
+
+  {
+    title: "Geirangerfjord",
+    location: "Norway",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "01 Oct, 2021 ",
+    endDate: "8 Nov, 2021",
+    description:
+      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1661345441183-d3d10b1f4e97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
+  },
+
+  {
+    title: "Cristo Redentor",
+    location: "Brazil",
+    googleMapsUrl: "https://goo.gl/maps/J6zjoK2NXQRJGA2D7",
+    startDate: "18 May, 2018 ",
+    endDate: "28 May, 2018",
+    description:
+      "Christ the Redeemer, Portuguese Cristo Redentor, colossal statue of Jesus Christ at the summit of Mount Corcovado, Rio de Janeiro, southeastern Brazil. Celebrated in traditional and popular songs.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1606141923451-000827e3c16e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+  },
+];
