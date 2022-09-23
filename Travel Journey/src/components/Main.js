4a511dae8e894904239f40df4500a0b15fa1a9b6
Emre Gundogan <emregundogan5@gmail.com>
2022-09-23 16:48:18 +0300

    first commit

---
+++ Travel Journey/src/components/Main.js
@@ -0,0 +1,30 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+import React from "react";
+import image from "../images/location-icon.png";
+
+export default function Main(props) {
+  return (
+    <div className="main--holder">
+      <div className="image--holder">
+        <img className="image" src={props.item.imageUrl} alt="" />
+      </div>
+
+      <div className="journey--details">
+        <div className="location--holder">
+          <img className="location--icon" src={image} alt="" />
+          <h3 className="country">{props.item.location}</h3>
+          <a href={props.item.googleMapsUrl} className="map--location"></a>
+        </div>
+
+        <h1 className="title">{props.item.title}</h1>
+
+        <div>
+          <h3 className="date">
+            {props.item.startDate} - {props.item.endDate}
+          </h3>
+          <p className="journey--text">{props.item.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
