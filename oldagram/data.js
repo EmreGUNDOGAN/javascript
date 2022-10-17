7f466cb3a59baabcc060a6d42469bb100c34fd50
Emre Gundogan <emregundogan5@gmail.com>
2022-10-17 15:17:42 +0300

    first commit

---
+++ oldagram/data.js
@@ -0,0 +1,35 @@
+const posts = [
+  {
+    name: "Vincent van Gogh",
+    username: "vincey1853",
+    location: "Zundert, Netherlands",
+    avatar: "images/user-avatar1.png",
+    post: "images/post-image1.png",
+    comment: "just took a few mushrooms lol",
+    likes: 21,
+    isLiked: true
+  },
+  {
+    name: "Gustave Courbet",
+    username: "gus1819",
+    location: "Ornans, France",
+    avatar: "images/user-avatar2.png",
+    post: "images/post-image2.png",
+    comment: "i'm feelin a bit stressed tbh",
+    likes: 4,
+    isLiked: false
+  },
+  {
+    name: "Joseph Ducreux",
+    username: "jd1735",
+    location: "Paris, France",
+    avatar: "images/user-avatar3.png",
+    post: "images/post-image3.png",
+    comment:
+      "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+    likes: 152,
+    isLiked: true
+  },
+];
+
+export default posts;
