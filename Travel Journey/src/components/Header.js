62a2f2b515b68de1fcf68c3cbec82af394dc1755
Emre Gundogan <emregundogan5@gmail.com>
2022-11-13 16:31:50 +0300

    fixed typo

--- Travel Journey/src/components/Header.js
+++ Travel Journey/src/components/Header.js
@@ -4,7 +4,7 @@ import logo from "../images/journey-logo.png";
 export default function Header() {
   return (
     <div className="header">
-     <img className="header--logo" src={logo} alt="" />
+      <img className="header--logo" src={logo} alt="" />
       <h2 className="header--title">my travel journal.</h2>
     </div>
   );
