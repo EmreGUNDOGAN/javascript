9201922701abc13846e03893f4ffe876fb636e8b
Emre Gundogan <emregundogan5@gmail.com>
2022-11-14 20:58:45 +0300

    added more data

--- Travel Journey/src/components/Data.js
+++ Travel Journey/src/components/Data.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default [
   {
+    id: 1,
     title: "Mount Fuji",
     location: "Japan",
     googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
@@ -11,6 +12,7 @@ export default [
     imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
   },
   {
+    id: 2,
     title: "Sydney Opera House",
     location: "Australia",
     googleMapsUrl: "https://goo.gl/maps/Gq7xEfWt3GmHSXWs7",
@@ -23,6 +25,7 @@ export default [
   },
 
   {
+    id: 3,
     title: "Geirangerfjord",
     location: "Norway",
     googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
@@ -35,6 +38,7 @@ export default [
   },
 
   {
+    id: 4,
     title: "Cristo Redentor",
     location: "Brazil",
     googleMapsUrl: "https://goo.gl/maps/J6zjoK2NXQRJGA2D7",
@@ -44,5 +48,99 @@ export default [
       "Christ the Redeemer, Portuguese Cristo Redentor, colossal statue of Jesus Christ at the summit of Mount Corcovado, Rio de Janeiro, southeastern Brazil. Celebrated in traditional and popular songs.",
     imageUrl:
       "https://images.unsplash.com/photo-1606141923451-000827e3c16e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+  },
+  {
+    id: 5,
+    title: "Lima",
+    location: "Peru",
+    googleMapsUrl: "https://goo.gl/maps/x1MWmUYSXtPrRt3aA",
+    startDate: "12 Jan, 2022",
+    endDate: "24 Jan, 2022",
+    description:
+      "Lima, the capital of Peru, lies on the country's arid Pacific coast. Though its colonial center is preserved, it's a bustling metropolis and one of South America’s largest cities.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1531968455001-5c5272a41129?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1206&q=80",
+  },
+  {
+    id: 6,
+    title: "San Blas Islands",
+    location: "Panama",
+    googleMapsUrl: "https://goo.gl/maps/BkTRYM58cPhTQcbw9",
+    startDate: "10 Jan, 2022",
+    endDate: "10 Jan, 2022",
+    description:
+      "The San Blas Islands is an archipelago in the Caribbean Sea is part of the comarca Guna Yala comprising approximately 365 islands and cays.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1611946022552-d2ca5ffba186?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+  },
+  {
+    id: 7,
+    title: "La Parguera",
+    location: "Puerto Rico",
+    googleMapsUrl: "https://goo.gl/maps/Bq8HguGHFWs8a25U9",
+    startDate: "12 Apr, 2019",
+    endDate: "24 Apr, 2019",
+    description:
+      "La Parguera bio bay is located on Puerto Rico's southern edge. This is Puerto Rico's unique bioluminescent bay where visitors may swim among the shimmering plankton. The bioluminescence is significantly more visible in the water when wearing a mask (compared to viewing from out of the water).",
+    imageUrl: "https://denomades.imgix.net/destinos/san-juan/1085/bay-san-juan.jpg?w=750&h=408&fit=crop&q=auto",
+  },
+  {
+    id: 8,
+    title: "Akihabara",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/rsYJs3ruzPynPRY29",
+    startDate: "12 Jan, 2018",
+    endDate: "24 Jan, 2018",
+    description:
+      "Akihabara is a district in central Tokyo that is famous for its many electronics shops and has also gained recognition as the center of Japan's otaku (diehard fan) culture. ",
+    imageUrl: "https://www.kanpai-japan.com/sites/default/files/styles/og/public/uploads/2015/09/akihabara-02.jpg",
+  },
+  {
+    id: 9,
+    title: "Berlin",
+    location: "Germany",
+    googleMapsUrl:
+      "https://www.google.com/maps/d/viewer?mid=1vYn8kYZ8dSBnLj85_rwPdJY_PH0&hl=en_US&ll=52.51898220723124%2C13.397010499999986&z=13",
+    startDate: "12 Jan, 2023",
+    endDate: "24 Jan, 2023",
+    description:
+      "Berlin, Germany’s capital, dates to the 13th century. Reminders of the city's turbulent 20th-century history include its Holocaust memorial and the Berlin Wall's graffitied remains. Divided during the Cold War, its 18th-century Brandenburg Gate has become a symbol of reunification.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1622182438074-b163bdf8f16c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=762&q=80",
+  },
+  {
+    id: 10,
+    title: "Fjaðrárgljúfur",
+    location: "Iceland",
+    googleMapsUrl:
+      "https://www.google.com/maps/d/viewer?mid=1vYn8kYZ8dSBnLj85_rwPdJY_PH0&hl=en_US&ll=63.77697320000004%2C-18.176124100000006&z=13",
+    startDate: "07 May, 2023",
+    endDate: "10 May, 2023",
+    description:
+      "Iceland has got some spectacular canyons, one of which is Fjaðrárgljúfur canyon in South-Iceland. Fjaðrárgljúfur is a canyon with quite a unique serpent-like shape and it was not well-known to visitors to Iceland until a few years ago. Don't worry if you cannot pronounce it, it is even difficult for Icelanders to pronounce.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1580424379900-36d35571c56c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80",
+  },
+  {
+    id: 11,
+    title: "Pão de Açúcar",
+    location: "Brazil",
+    googleMapsUrl: "https://goo.gl/maps/9tTQjo2QFUpKyxKZ7",
+    startDate: "12 Apr, 2022",
+    endDate: "16 Apr, 2022",
+    description:
+      "Pão de Açúcar is a complex of hills located in the neighborhood of Urca, in the city of Rio de Janeiro, Brazil. It is one of the biggest postcard in the city of Rio de Janeiro and one of the most famous in Brazil.",
+    imageUrl: "https://source.unsplash.com/kGPe6Dc_t1A",
+  },
+  {
+    id: 12,
+    title: "Farol da Barra",
+    location: "Brazil",
+    googleMapsUrl: "https://goo.gl/maps/EhicN5F41GzR6Uai9",
+    startDate: "5 Jul, 2022",
+    endDate: "7 Jul, 2022",
+    description:
+      "Farol da Barra is located in Salvador, on the coast of the state of Bahia. It was the second nautical signaling system to come into operation in Brazil, after the lighthouse of the Friburgo Palace in Recife.",
+    imageUrl: "https://source.unsplash.com/2o05Oktqsss",
   },
 ];
