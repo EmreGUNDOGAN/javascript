dde8dc17b807593f61c30f7909f16a60fc929fd9
Emre Gundogan <emregundogan5@gmail.com>
2022-11-14 18:13:00 +0300

    first commit

---
+++ AirBnb Experiences Clone/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return(
+        <header className="header">
+        <img src="../images/airbnb-logo.png" alt="airbnb logo" className="logo" />
+        </header>
+    )
+}
+
+export default Header
