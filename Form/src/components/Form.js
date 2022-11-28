d6059d52a75501f3af2ee22f19f9434c24535f41
Emre Gundogan <emregundogan5@gmail.com>
2022-11-28 18:47:31 +0300

    first commit

---
+++ Form/src/components/Form.js
@@ -0,0 +1,90 @@
+import React from "react";
+
+
+export default function Form(){
+
+    const [formData, setFormData] = React.useState(
+    {        
+        email: "",
+        password: "",
+        confirmPassword : "",
+        isChecked: true
+    }
+        )
+        console.log(formData);
+    function handleChange(event){
+        const {name, value, type, checked} = event.target
+        setFormData(prevForm => {
+            return {
+                ...prevForm,
+                [name]: type === "checkbox" ? checked : value,
+            };
+        })
+    }
+    
+    function submitForm(event){
+        event.preventDefault()
+        if(formData.password === formData.confirmPassword){
+            alert("Successfully signed up");
+            console.log("Successfully signed up");
+        } else {
+            alert("Passwords does not match!");
+            console.log("Passwords does not match!")
+        }
+    }
+
+
+    return (
+    <div className="container">
+        <form onSubmit={submitForm} className="form">
+            <input
+                type="text"
+                value={formData.email}
+                name="email"
+                onChange={handleChange}
+                placeholder="Email address"
+                className="form--input"
+                required
+        />
+
+            <input
+                type="password"
+                value={formData.password}
+                name="password"
+                onChange={handleChange}
+                placeholder="Password"
+                className="form--input"
+                required
+        />
+
+            <input
+                type="password"
+                value={formData.confirmPassword}
+                name="confirmPassword"
+                onChange={handleChange}
+                placeholder="Confirm password"
+                className="form--input"
+                required
+        />
+
+        <div className="marketing">
+            <input
+                type="checkbox"
+                className="newsletter-checkbox"
+                checked={formData.isChecked}
+                name="isChecked"
+                onChange={handleChange}
+                id="newsletter"
+            />
+
+            <label className="newsletter-txt" htmlFor="newsletter">
+                I want to join the newsletter
+            </label>
+        </div>
+
+            <button>Sign up</button>
+
+        </form>
+    </div>
+    );
+}
