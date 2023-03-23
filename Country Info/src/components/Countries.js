10dfd169cd2443e5e1842617cf4cf057eed94215
Emre Gündoğan <emregundogan5@gmail.com>
2023-03-23 18:51:01 +0300

    deleted unused comments, error message for users

--- Country Info/src/components/Countries.js
+++ Country Info/src/components/Countries.js
@@ -1,8 +1,5 @@
 import React from "react";
-import Card from "../Card";
-
-
-// error handle, try if oluştur. ! ise fetch yapma.
+import Card from "./Card";
 
 export default function Countries() {
 
@@ -10,14 +7,13 @@ export default function Countries() {
         userInput: ""
     })
 
-    
     const [country, setCountry] = React.useState({});
 
     const [active, setActive] = React.useState(false)
   
-        async function fetchData(countryName= "canada") {
+        async function fetchData(countryName= "germany") {
           try {
-          const res = await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+          const res = await fetch(`https://restcountries.com/v3.1/name/${countryName} `)
           const data = await res.json()
           console.log(data[0]);
           setCountry(data[0])
@@ -45,22 +41,32 @@ export default function Countries() {
       event.preventDefault()
       setActive(true)
       fetchData(input.userInput)
-
+      
     }
 
-    console.log(country)
-
     return (
-      
+      <>
+        <p className="error-message">
+          Due to API issues make sure to enter valid country name, on any error refresh the page.
+        </p>
         <div className="container">
+
           <form action="" onSubmit={submitBtn}>
-            <input type="text" name="userInput" value={input.userInput} onChange={handleChange} placeholder="e.g Germany" />
+            <input
+              required
+              type="text"
+              name="userInput"
+              value={input.userInput}
+              onChange={handleChange}
+              placeholder="e.g Germany"
+            />
+
             <button>Search</button>
           </form>
 
           {!active && <p>Loading</p>}
           {active && <Card country={country} />}
         </div>
-      
+      </>
     );
 }
