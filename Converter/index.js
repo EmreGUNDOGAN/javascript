dcabf5da09b119df21a26ee86507d173239eff81
Emre Gündoğan <emregundogan5@gmail.com>
2023-04-28 17:54:47 +0300

    fixed broken codes due misspell

--- Converter/index.js
+++ Converter/index.js
@@ -1,34 +1,32 @@
-let metersEl = document.getElementById("meters");
+let metersEl = document.getElementById("meter");
 let feetEl = document.getElementById("feet");
-let gallonsEl = document.getElementById("gallons");
-let litersEl = document.getElementById("liters");
-let poundsEl = document.getElementById("pounds");
-let kilosEl = document.getElementById("kilos");
+let gallonsEl = document.getElementById("gallon");
+let litersEl = document.getElementById("liter");
+let poundsEl = document.getElementById("pound");
+let kilosEl = document.getElementById("kilo");
 
 function convert(valueNum) {
   // Meters to Feet
-
-  let metersValue = (valueNum * 3.2808399).toFixed(3);
+  let metersValue = (valueNum * 3.2808399).toFixed(2);
   metersEl.textContent = `${valueNum} meters = ${metersValue} feets`;
 
   //   Feet to Meters
-
-  let feetValue = (valueNum / 3.2808399).toFixed(3);
+  let feetValue = (valueNum / 3.2808399).toFixed(2);
   feetEl.textContent = `${valueNum} feet = ${feetValue} meters`;
 
   //   Liters to Gallons
-  let literValue = (valueNum / 3.78541178).toFixed(3);
+  let literValue = (valueNum / 3.78541178).toFixed(2);
   litersEl.textContent = `${valueNum} liters = ${literValue} gallons`;
 
   //  Gallons to Liters
-  let gallonValue = (valueNum * 3.78541178).toFixed(3);
+  let gallonValue = (valueNum * 3.78541178).toFixed(2);
   gallonsEl.textContent = `${valueNum} gallons = ${gallonValue} liters`;
 
   //  Kilos to Pound
-  let kiloValue = (valueNum * 2.20462262).toFixed(3);
+  let kiloValue = (valueNum * 2.20462262).toFixed(2);
   kilosEl.textContent = `${valueNum} kilos = ${kiloValue} pound`;
 
   // Pounds to Kilos
-  let poundValue = (valueNum / 2.20462262).toFixed(3);
+  let poundValue = (valueNum / 2.20462262).toFixed(2);
   poundsEl.textContent = `${valueNum} pounds = ${poundValue} kilos`;
 }
